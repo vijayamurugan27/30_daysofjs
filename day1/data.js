fbb85d9521622407a957b8c0b6d75170169d8f01
vijayamurugan27 <vijayamurugan27@gmail.com>
2022-08-22 14:22:54 +0530

    Add files via upload

---
+++ day1/data.js
@@ -0,0 +1,11 @@
+let name = "john"
+let job = "Web developer"
+let salary = 75000
+
+const bp = " 120/60 "
+const BP = 100
+const gravity = 9.81
+
+var name1 = 'raju'
+var job1 = 'Teacher'
+var salary1 = 20000
---
+++ day1/main.js
@@ -0,0 +1,11 @@
+console.log("Name = ",name);
+console.log("job = ", job);
+console.log(" Salary = ", salary);
+
+document.write(bp)
+document.write(BP)
+document.write(gravity)
+
+console.warn(name1)
+console.error(job1)
+console.log(salary1)
