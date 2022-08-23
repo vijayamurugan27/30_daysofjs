ccf12e07c2d6771d0c3aeadf8dd28e129111ae30
vijayamurugan27 <vijayamurugan27@gmail.com>
2022-08-23 14:12:42 +0530

    Add files via upload

---
+++ Day_4/conditionals.js
@@ -0,0 +1,107 @@
+let num = 3
+if (num > 0) {
+  console.log(`${num} is a positive number`)
+}
+//  3 is a positive number
+let isRaining = true
+if (isRaining) {
+  console.log('Remember to take your rain coat.')
+}
+
+console.log("if else - condition")
+
+let num1 = 3
+if (num1 > 0) {
+  console.log(`${num1} is a positive number`)
+} else {
+  console.log(`${num1} is a negative number`)
+}
+//  3 is a positive number
+
+let num2 = -3
+if (num2 > 0) {
+  console.log(`${num2} is a positive number`)
+} else {
+  console.log(`${num2} is a negative number`)
+}
+//  -3 is a negative number
+
+let a = 0
+if (a > 0) {
+  console.log(`${a} is a positive number`)
+} else if (a < 0) {
+  console.log(`${a} is a negative number`)
+} else if (a == 0) {
+  console.log(`${a} is zero`)
+} else {
+  console.log(`${a} is not a number`)
+}
+
+console.log("Switch statements")
+
+let weather = 'cloudy'
+switch (weather) {
+  case 'rainy':
+    console.log('You need a rain coat.')
+    break
+  case 'cloudy':
+    console.log('It might be cold, you need a jacket.')
+    break
+  case 'sunny':
+    console.log('Go out freely.')
+    break
+  default:
+    console.log(' No need for rain coat.')
+}
+
+// Switch More Examples
+let dayUserInput = prompt('What day is today ?')
+let day = dayUserInput.toLowerCase()
+
+switch (day) {
+  case 'monday':
+    console.log('Today is Monday')
+    break
+  case 'tuesday':
+    console.log('Today is Tuesday')
+    break
+  case 'wednesday':
+    console.log('Today is Wednesday')
+    break
+  case 'thursday':
+    console.log('Today is Thursday')
+    break
+  case 'friday':
+    console.log('Today is Friday')
+    break
+  case 'saturday':
+    console.log('Today is Saturday')
+    break
+  case 'sunday':
+    console.log('Today is Sunday')
+    break
+  default:
+    console.log('It is not a week day.')
+}
+// Examples to use conditions in the cases
+
+let num_a = prompt('Enter number');
+switch (true) {
+  case num_a > 0:
+    console.log('Number is positive');
+    break;
+  case num_a == 0:
+    console.log('Numbers is zero');
+    break;
+  case num_a < 0:
+    console.log('Number is negative');
+    break;
+  default:
+    console.log('Entered value was not a number');
+}
+
+// Ternary Operators
+let isRaining_2 = true
+isRaining_2
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
